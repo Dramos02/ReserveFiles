b2129e80464bd35ca04718839fbd859e4c4514a3
Dominic Ramos <106921028+Dramos02@users.noreply.github.com>
2023-12-13 08:51:20 +0800

    Add files via upload

--- ravi(htdocs)FIREBASE-PHP/action_add.php
+++ ravi(htdocs)FIREBASE-PHP/action_add.php
@@ -5,26 +5,37 @@ include("config.php");
 include("firebaseRDB.php");
 $db = new firebaseRDB($databaseURL);
 
-$insert = $db ->insert("ravi",
-[
-    "fullname" => $_POST['fullname'],
-    "email" => $_POST['email'],
-    "contactnumber" => $_POST['contactnumber'],
-    "streetaddress" => $_POST['streetaddress'],
-    "city" => $_POST['city'],
-    "stateprovince" => $_POST['stateprovince'],
-    "postalcode" => $_POST['postalcode'],
-    "minbudget" => $_POST['minbudget'],
-    "maxbudget" => $_POST['maxbudget'],
-    
+$raviData = [
+  "initial" => [
+    "initial_name" => $_POST['initial_name'],
+    "initial_contactnumber" => $_POST['initial_contactnumber'],
+    "initial_address" => $_POST['initial_address'],
+    "initial_idprofFee" => $_POST['initial_idprofFee'],
+    "initial_profession" => $_POST['initial_profession']
+  ],
+  "primary" => [
+    "primary_name" => $_POST['primary_name'],
+    "primary_contactnumber" => $_POST['primary_contactnumber'],
+    "primary_address" => $_POST['primary_address'],
+    "primary_idprofFee" => $_POST['primary_idprofFee'],
+    "primary_profession" => $_POST['primary_profession']
+  ],
+  "owner" => [
+    "owner_name" => $_POST['owner_name'],
+    "owner_contactnumber" => $_POST['owner_contactnumber'],
+    "owner_address" => $_POST['owner_address'],
+    "owner_idprofFee" => $_POST['owner_idprofFee'],
+    "owner_profession" => $_POST['owner_profession']
+  ]
+];
 
-]);
+$insert = $db->insert("ravi", $raviData);
 
-if($insert){
-    $_SESSION['status'] ="Inquries submitted, Wait for our call till we Verify you!";
-    header('Location: index.php');
-}else {
-    $_SESSION['status'] ="Some Went Wrong!! Check Backend...";
-    header('Location: index.php');
+if ($insert) {
+  $_SESSION['status'] = "Inquries submitted, Wait for our call till we Verify you!";
+  header('Location: index.php');
+} else {
+  $_SESSION['status'] = "Some Went Wrong!! Check Backend...";
+  header('Location: index.php');
 }
 ?>
